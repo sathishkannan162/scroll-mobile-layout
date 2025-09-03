4bd45cfa0e1360517deeb0f1ced792dac4d5e164
Sathish K M <sathish.km@iouring.com>
2025-09-03 16:19:57 +0530

    Fix scroll top values for scrollable headings

--- app/overview/sticky-scroll.tsx
+++ app/overview/sticky-scroll.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useRef } from "react";
 const StickyScroll = () => {
   const headerRef = useRef<HTMLDivElement>(null);
   const stockContentRef = useRef<HTMLDivElement>(null);
+  const scrollableHeadingsRef = useRef<HTMLDivElement>(null);
   const lastScrollY = useRef<number>(window.scrollY);
 
   useEffect(() => {
@@ -11,7 +12,12 @@ const StickyScroll = () => {
       const currentScrollY = window.scrollY;
       const isScrollingDown = currentScrollY > lastScrollY.current;
 
-      if (!stockContentRef.current || !headerRef.current) return;
+      if (
+        !stockContentRef.current ||
+        !headerRef.current ||
+        !scrollableHeadingsRef.current
+      )
+        return;
 
       if (isScrollingDown) {
         // When scrolling up, Stock Content sticks to top-0
@@ -19,12 +25,16 @@ const StickyScroll = () => {
         stockContentRef.current.classList.remove("top-10");
         headerRef.current.classList.add("-top-10");
         headerRef.current.classList.remove("top-0");
+        scrollableHeadingsRef.current.classList.add("top-[96px]");
+        scrollableHeadingsRef.current.classList.remove("top-[136px]");
       } else {
         // When scrolling down, Header sticks to top-0, Stock Content below it
         headerRef.current.classList.add("top-0");
         headerRef.current.classList.remove("-top-10");
         stockContentRef.current.classList.add("top-10");
         stockContentRef.current.classList.remove("top-0");
+        scrollableHeadingsRef.current.classList.add("top-[136px]");
+        scrollableHeadingsRef.current.classList.remove("top-10");
       }
 
       lastScrollY.current = currentScrollY;
@@ -50,7 +60,10 @@ const StickyScroll = () => {
         >
           Stock Content
         </div>
-        <div className="w-full bg-gray-100 h-24 sticky top-34 transition-all duration-200 z-0">
+        <div
+          ref={scrollableHeadingsRef}
+          className="w-full bg-gray-100 h-24 sticky top-[136px] transition-all duration-200 z-0"
+        >
           Scrollable headings
         </div>
         <div className="w-full bg-gray-400 h-[1000px]">Content</div>
