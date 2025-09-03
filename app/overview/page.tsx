c4c8c44fb7eeed51bb821ea4947cbc0bb9b2af97
Sathish K M <sathish.km@iouring.com>
2025-09-03 15:37:12 +0530

    Make a scroll layout with fixed headers

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -1,3 +1,17 @@
 export default function Overview() {
-  return <div>Hello</div>;
+  return (
+    <div className="bg-white">
+      <div>
+        <div className="w-full bg-gray-200 h-10 sticky top-0">Header</div>
+        <div className="w-full bg-gray-100 h-10">Breadcrumbs</div>
+        <div className="w-full bg-gray-300 h-24 sticky top-0">
+          Stock Content
+        </div>
+        <div className="w-full bg-gray-100 h-24 sticky top-24">
+          Scrollable headings
+        </div>
+        <div className="w-full bg-gray-400 h-[1000px]">Content</div>
+      </div>
+    </div>
+  );
 }
