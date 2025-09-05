a697a4f28f8b5e046fb0b210d4b3fa6751f014bf
Sathish K M <sathish.km@iouring.com>
2025-09-05 16:53:23 +0530

    Refactor table

--- app/components/StockTable.tsx
+++ app/components/StockTable.tsx
@@ -1,85 +1,91 @@
 export default function StockTable() {
+  const stockData = [
+    {
+      symbolMain: "RELIANCE 30SEP FUT",
+      symbolSub: "NFO",
+      ltpValue: "1378.00",
+      ltpChange: "+12.10 (+0.88%)",
+      premDis: "+3.00",
+      oi: "12,23,24,000",
+      oiChangeValue: "-10,95,000",
+      oiChangePercent: "(-0.89%)",
+      high: "1384.50",
+      low: "1362.70",
+      volume: "1,04,32,000",
+    },
+    {
+      symbolMain: "RELIANCE 28OCT FUT",
+      symbolSub: "NFO",
+      ltpValue: "1385.00",
+      ltpChange: "+12.20 (+0.88%)",
+      premDis: "+10.00",
+      oi: "97,94,500",
+      oiChangeValue: "-21,000",
+      oiChangePercent: "(-0.21%)",
+      high: "1390.40",
+      low: "1370.00",
+      volume: "7,13,500",
+    },
+    {
+      symbolMain: "RELIANCE 25NOV FUT",
+      symbolSub: "NFO",
+      ltpValue: "1391.90",
+      ltpChange: "+12.40 (+0.89%)",
+      premDis: "+16.90",
+      oi: "7,52,500",
+      oiChangeValue: "+34,000",
+      oiChangePercent: "(+4.73%)",
+      high: "1397.00",
+      low: "1376.80",
+      volume: "1,42,500",
+    },
+  ];
+
+  const headers = [
+    "Symbol",
+    "LTP",
+    "Prem/Dis",
+    "OI",
+    "OI Change",
+    "High",
+    "Low",
+    "Volume",
+  ];
+
   return (
     <table>
       <thead>
         <tr>
-          <th>Symbol</th>
-          <th>LTP</th>
-          <th>Prem/Dis</th>
-          <th>OI</th>
-          <th>OI Change</th>
-          <th>High</th>
-          <th>Low</th>
-          <th>Volume</th>
+          {headers.map((header, index) => (
+            <th key={index}>{header}</th>
+          ))}
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>
-            RELIANCE 30SEP FUT
-            <br />
-            NFO
-          </td>
-          <td>
-            1378.00
-            <br />
-            +12.10 (+0.88%)
-          </td>
-          <td>+3.00</td>
-          <td>12,23,24,000</td>
-          <td>
-            -10,95,000
-            <br />
-            (-0.89%)
-          </td>
-          <td>1384.50</td>
-          <td>1362.70</td>
-          <td>1,04,32,000</td>
-        </tr>
-        <tr>
-          <td>
-            RELIANCE 28OCT FUT
-            <br />
-            NFO
-          </td>
-          <td>
-            1385.00
-            <br />
-            +12.20 (+0.88%)
-          </td>
-          <td>+10.00</td>
-          <td>97,94,500</td>
-          <td>
-            -21,000
-            <br />
-            (-0.21%)
-          </td>
-          <td>1390.40</td>
-          <td>1370.00</td>
-          <td>7,13,500</td>
-        </tr>
-        <tr>
-          <td>
-            RELIANCE 25NOV FUT
-            <br />
-            NFO
-          </td>
-          <td>
-            1391.90
-            <br />
-            +12.40 (+0.89%)
-          </td>
-          <td>+16.90</td>
-          <td>7,52,500</td>
-          <td>
-            +34,000
-            <br />
-            (+4.73%)
-          </td>
-          <td>1397.00</td>
-          <td>1376.80</td>
-          <td>1,42,500</td>
-        </tr>
+        {stockData.map((stock, index) => (
+          <tr key={index}>
+            <td>
+              {stock.symbolMain}
+              <br />
+              {stock.symbolSub}
+            </td>
+            <td>
+              {stock.ltpValue}
+              <br />
+              {stock.ltpChange}
+            </td>
+            <td>{stock.premDis}</td>
+            <td>{stock.oi}</td>
+            <td>
+              {stock.oiChangeValue}
+              <br />
+              {stock.oiChangePercent}
+            </td>
+            <td>{stock.high}</td>
+            <td>{stock.low}</td>
+            <td>{stock.volume}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
